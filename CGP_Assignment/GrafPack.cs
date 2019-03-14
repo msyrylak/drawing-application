19578b34db682825b431650828f8ae03a0eee81b
msyrylak <msyrylak@gmail.com>
2019-03-14 15:39:37 +0000

    Add project files.

---
+++ CGP_Assignment/GrafPack.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CGP_Assignment
+{
+    public partial class GrafPack : Form
+    {
+
+        private MainMenu mainMenu;
+        private bool selectSquareStatus = false;
+        private bool selectTriangleStatus = false;
+
+        private int clicknumber = 0;
+        private Point one;
+        private Point two;
+
+        public GrafPack()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.WindowState = FormWindowState.Maximized;
+            this.BackColor = Color.White;
+
+            // The following approach uses menu items coupled with mouse clicks
+            MainMenu mainMenu = new MainMenu();
+            MenuItem createItem = new MenuItem();
+            MenuItem selectItem = new MenuItem();
+            MenuItem squareItem = new MenuItem();
+            MenuItem triangleItem = new MenuItem();
+
+            createItem.Text = "&Create";
+            squareItem.Text = "&Square";
+            triangleItem.Text = "&Triangle";
+            selectItem.Text = "&Select";
+            
+            mainMenu.MenuItems.Add(createItem);
+            mainMenu.MenuItems.Add(selectItem);
+            createItem.MenuItems.Add(squareItem);
+            createItem.MenuItems.Add(triangleItem);
+
+            selectItem.Click += new System.EventHandler(this.selectShape);
+            squareItem.Click += new System.EventHandler(this.selectSquare);
+            triangleItem.Click += new System.EventHandler(this.selectTriangle);
+
+            this.Menu = mainMenu;
+            this.MouseClick += mouseClick;
+        }
+
+        // Generally, all methods of the form are usually private
+        private void selectSquare(object sender, EventArgs e)
+        {
+            selectSquareStatus = true;
+            MessageBox.Show("Click OK and then click once each at two locations to create a square");
+        }
+
+        private void selectTriangle(object sender, EventArgs e)
+        {
+            selectTriangleStatus = true;
+        }
+
+        private void selectShape(object sender, EventArgs e)
+        {
+            MessageBox.Show("You selected the Select option...");
+        }
+
+        // This method is quite important and detects all mouse clicks - other methods may need
+        // to be implemented to detect other kinds of event handling eg keyboard presses.
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // 'if' statements can distinguish different selected menu operations to implement.
+                // There may be other (better, more efficient) approaches to event handling,
+                // but this approach works.
+                if (selectSquareStatus == true)
+                {
+                    if (clicknumber == 0)
+                    {
+                        one = new Point(e.X, e.Y);
+                        clicknumber = 1;
+                    }
+                    else
+                    {
+                        two = new Point(e.X, e.Y);
+                        clicknumber = 0;
+                        selectSquareStatus = false;
+
+                        Graphics g = this.CreateGraphics();
+                        Pen blackpen = new Pen(Color.Black);
+
+                        Square aShape = new Square(one, two);
+                        aShape.draw(g, blackpen);
+                    }
+                }
+            }
+        }
+    }
+    
+    abstract class Shape
+    {
+        // This is the base class for Shapes in the application. It should allow an array or LL
+        // to be created containing different kinds of shapes.
+        public Shape()   // constructor
+        {            
+        }
+    }
+    
+    class Square : Shape
+    {
+        //This class contains the specific details for a square defined in terms of opposite corners
+        Point keyPt, oppPt;      // these points identify opposite corners of the square
+
+        public Square(Point keyPt, Point oppPt)   // constructor
+        {
+            this.keyPt = keyPt;
+            this.oppPt = oppPt;
+        }
+
+        // You will need a different draw method for each kind of shape. Note the square is drawn
+        // from first principles. All other shapes should similarly be drawn from first principles. 
+        // Ideally no C# standard library class or method should be used to create, draw or transform a shape
+        // and instead should utilse user-developed code.
+	    public void draw(Graphics g, Pen blackPen)
+        {
+            // This method draws the square by calculating the positions of the other 2 corners
+            double xDiff, yDiff, xMid, yMid;   // range and mid points of x & y  
+
+            // calculate ranges and mid points
+            xDiff = oppPt.X - keyPt.X;
+            yDiff = oppPt.Y - keyPt.Y;
+            xMid = (oppPt.X + keyPt.X) / 2;
+            yMid = (oppPt.Y + keyPt.Y) / 2;
+
+            // draw square
+            g.DrawLine(blackPen, (int)keyPt.X, (int)keyPt.Y, (int)(xMid + yDiff / 2), (int)(yMid - xDiff / 2));
+            g.DrawLine(blackPen, (int)(xMid + yDiff / 2), (int)(yMid - xDiff / 2), (int)oppPt.X, (int)oppPt.Y);
+            g.DrawLine(blackPen, (int)oppPt.X, (int)oppPt.Y, (int)(xMid - yDiff / 2), (int)(yMid + xDiff / 2));
+            g.DrawLine(blackPen, (int)(xMid - yDiff / 2), (int)(yMid + xDiff / 2), (int)keyPt.X, (int)keyPt.Y);
+        }
+
+    }
+}
+
+
